70fd268da0372c3f0e52042db9472bb122a1fea2
Sh4d0w <266088743+unkn0wn-sh4d0w@users.noreply.github.com>
2026-03-30 20:21:45 +0200

    Update send.js

--- api/send.js
+++ api/send.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
     return res.status(403).json({ error: "Forbidden" });
   }
 
-  res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
