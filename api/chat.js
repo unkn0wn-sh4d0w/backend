7e2029cc957164d0d9054d60f17d976146674575
Sh4d0w <266088743+unkn0wn-sh4d0w@users.noreply.github.com>
2026-03-31 18:35:07 +0200

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,7 +1,12 @@
-import { kv } from "@vercel/kv";
+import { Redis } from "@upstash/redis";
+
+const redis = new Redis({
+  url: process.env.KV_REST_API_URL,
+  token: process.env.KV_REST_API_TOKEN
+});
 
 export default async function handler(req, res) {
-  const data = await kv.lrange("messages", 0, 100);
+  const data = await redis.lrange("chat_messages", 0, 100);
   const messages = data.map(m => JSON.parse(m)).reverse();
   res.status(200).json(messages);
 }
