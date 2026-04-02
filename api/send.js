cddbc541862fe702686c487aa7aa79c2624ff787
Sh4d0w <266088743+unkn0wn-sh4d0w@users.noreply.github.com>
2026-04-02 18:31:28 +0200

    Update send.js

--- api/send.js
+++ api/send.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, x-api-key");
 
   if (req.method === "OPTIONS") {
     return res.status(200).end();
@@ -10,6 +10,10 @@ export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).send("Only POST allowed");
   }
+
+if (req.headers['x-api-key'] !== process.env.API_SECRET) {
+  return res.status(403).json({ error: 'Forbidden' });
+}
 
   const { type, message, username } = req.body;
 
